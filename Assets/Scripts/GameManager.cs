d5ecac690aca63031b122ad22d0f33c2edd0bcd3
Sienna <animalsienna@gmail.com>
2017-02-24 22:00:04 -0500

    Began to implement skeleton of basic systems Player movement on click, scaling with z-depth Open/close inventory on right click

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    private static GameManager instance;
 
-	// Use this for initialization
+    public static WorldObject Player {
+        get { return instance.player; }
+    }
+    [SerializeField]
+    private WorldObject player;
+    [SerializeField]
+    private Inventory inventory;
+    
+
 	void Start () {
-		
+		if (instance == null) {
+            DontDestroyOnLoad(this);
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update() {
+        if (Input.GetMouseButtonUp(1)) {
+            if (Inventory.ItemSelected(null)) {
+                inventory.gameObject.SetActive(!inventory.gameObject.activeInHierarchy);
+            }
+            else {
+                Inventory.ClearSelection();
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+    void OnMouseUpAsButton() {
+        if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameManager.Player.MoveToPoint(pos);
+        }
+    }
+}
