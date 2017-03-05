412722ca95460e017f6404fdf08b04e0565599df
Sienna <animalsienna@gmail.com>
2017-03-05 18:48:45 -0500

    UI tweaks

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -2,16 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Attached to the background of a scene.
-/// </summary>
 public class World : MonoBehaviour {
-    /// <summary>
-    /// When the player clicks on the background, their character
-    /// should move toward the point they clicked.
-    /// </summary>
     void OnMouseUpAsButton() {
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        GameManager.Player.MoveToPoint(pos);
+        if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameManager.Player.MoveToPoint(pos);
+        }
     }
 }
