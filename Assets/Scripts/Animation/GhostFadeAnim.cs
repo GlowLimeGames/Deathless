7f1f256c944b8c2d674fb0394d6ea83da400c0ad
SiennaC <sc16@hampshire.edu>
2017-08-06 13:15:03 -0400

    Ghost fading anim.

---
+++ Assets/Scripts/Animation/GhostFadeAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostFadeAnim : MonoBehaviour {
+    [SerializeField]
+    private float fadeRate = 2f;
+    [SerializeField]
+    private float fadeDelay = 2f;
+
+    private bool fadeIn;
+    private System.DateTime delayStart;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        delayStart = System.DateTime.Now;
+    }
+
+    void Update() {Color color = spriteRenderer.color;
+        if (delayStart != default(System.DateTime) &&
+            (System.DateTime.Now - delayStart).TotalSeconds >= fadeDelay) {
+            delayStart = default(System.DateTime);
+        }
+
+        if (fadeIn) {
+            color.a += Time.deltaTime / fadeRate;
+            if (color.a > 0.99f) {
+                delayStart = System.DateTime.Now;
+                fadeIn = false;
+            }
+        }
+        else if (delayStart == default(System.DateTime)) {
+            color.a -= Time.deltaTime / fadeRate;
+            if (color.a < 0.01f) { fadeIn = true; }
+        }
+        spriteRenderer.color = color;
+    }
+}
