858eb3670e1ccc761ccd932360d51cddcd5c3dbe
SiennaC <sc16@hampshire.edu>
2017-09-21 19:15:59 -0400

    Updated project to Unity 2017.1.1f1 Updated Wwise project & integration to 2017.1.1.6340.770

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID INCINERATORAREASTART = 3256731940U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID INCINERATOR = 2193956003U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID INCINERATOR_AREA = 3696518466U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID SFX_MASTER = 3222323340U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
