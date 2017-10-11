1cc6628d75ae5f399131925c4865a123b8c98c58
Faith <faithkim1158@gmail.com>
2017-10-11 16:34:15 -0400

    also added testUnitEvent to test if audio is working

---
+++ Assets/testUnityEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class testUnityEvent : MonoBehaviour {
+    public UnityEvent testEvent; 
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        //testing unityevent
+		if ( Input.GetKeyDown( KeyCode.E ))
+        {
+            testEvent.Invoke();
+        }
+	}
+}
