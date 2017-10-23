ce3bddd06db0542e657ea21bf4a6894539a4c5c9
Sienna <animalsienna@gmail.com>
2017-10-22 23:44:06 -0400

    Small fixed to dialogue tree logic (links should not change nodedata transforms).

--- Assets/Scripts/Dialogue/Core/DialogueTree.cs
+++ Assets/Scripts/Dialogue/Core/DialogueTree.cs
@@ -47,7 +47,7 @@ namespace Dialogue {
             Data = data;
             if (Data != null) {
                 Data.ValidateComponents();
-                if (parent != null && parent.Data != null) {
+                if (!isLink && parent != null && parent.Data != null) {
                     Data.ValidateParent(parent.Data);
                 }
             }
@@ -74,7 +74,7 @@ namespace Dialogue {
             Parent.Children.Add(this);
 
             if (Data != null && Parent.Data != null) {
-                Data.gameObject.transform.SetParent(Parent.Data.gameObject.transform);
+                if (!isLink) { Data.gameObject.transform.SetParent(Parent.Data.gameObject.transform); }
             }
             else {
                 Debug.LogWarning("Failed to properly move data object " + Data + " to parent object " + Parent.Data + "./n" +
