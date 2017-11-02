2337e0c86d2999e5e38193e24d753427b79309d3
SiennaC <sc16@hampshire.edu>
2017-11-02 19:15:31 -0400

    Trying to make Unity Cloud Build PreBuild stuff work.

--- Assets/Scripts/Util/Editor/BuildProcessing.cs
+++ Assets/Scripts/Util/Editor/BuildProcessing.cs
@@ -2,26 +2,29 @@
 using System.IO;
 
 public static class BuildProcessing {
+    private const string WWISE_PROJECT_PATH = "../Deathless_WwiseProject/";
     private const string MAC = "Mac";
     private const string WINDOWS = "Windows";
 
     // PreBuild methods are called from Unity Cloud Build
     // Build target -> Advanced Options -> Pre-Export Method
+    [UnityEditor.MenuItem("File/Copy Wwise Soundbanks/Mac")]
     public static void PreBuildMac() { MoveSoundBanks(MAC); }
+
+    [UnityEditor.MenuItem("File/Copy Wwise Soundbanks/Windows")]
     public static void PreBuildWindows() { MoveSoundBanks(WINDOWS); }
 
     /// <summary>
     /// Based on AkExampleAppBuilderBase.Build
     /// </summary>
     private static void MoveSoundBanks(string wwisePlatformString) {
-        string wwiseProjFile = Path.Combine(Application.dataPath, WwiseSetupWizard.Settings.WwiseProjectPath).Replace('/', '\\');
-        string wwiseProjectFolder = wwiseProjFile.Remove(wwiseProjFile.LastIndexOf(Path.DirectorySeparatorChar));
-        
-        string sourceSoundBankFolder = Path.Combine(wwiseProjectFolder, AkBasePathGetter.GetPlatformBasePath());
+        Debug.Log("Wwise project path: " + WwiseSetupWizard.Settings.WwiseProjectPath);
+
+        string sourceSoundBankFolder = Path.Combine(WWISE_PROJECT_PATH, AkBasePathGetter.GetPlatformBasePath());
         string destinationSoundBankFolder = Path.Combine(Application.dataPath + Path.DirectorySeparatorChar + "StreamingAssets",
                                                                 Path.Combine(WwiseSetupWizard.Settings.SoundbankPath, wwisePlatformString));
 
-        Debug.Log("Moving soundbanks from " + sourceSoundBankFolder + " to " + destinationSoundBankFolder);
+        Debug.Log("Copying soundbanks from " + sourceSoundBankFolder + " to " + destinationSoundBankFolder);
         if (!AkUtilities.DirectoryCopy(sourceSoundBankFolder, destinationSoundBankFolder, true)) {
             Debug.LogError("WwiseUnity: The soundbank folder for the " + wwisePlatformString + " platform doesn't exist. Make sure it was generated in your Wwise project");
         }
