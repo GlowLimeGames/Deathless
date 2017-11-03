6ca9f1b9c2e40cbb11eacb4b38a51a3035067960
Faith <faithkim1158@gmail.com>
2017-11-03 17:25:58 -0400

    used update function to make popupinventory animation--but will change to coroutine function

--- Assets/Scripts/Items/PopupInventoryItem.cs
+++ Assets/Scripts/Items/PopupInventoryItem.cs
@@ -5,21 +5,48 @@ using UnityEngine.UI;
 
 public class PopupInventoryItem : MonoBehaviour {
     //make this into a queue or a list in case we need to process more than one image
+    //currently trying to work with one item 
+    //i might just be able to make this a local variable to renderItemSprite
     private static Image gameObjectImage;
-    //possibly create a queue
-    //also check for condition if currently in a dialogue 
+    private static bool itemRendered = false;
+    int counter = 0;
+    int animationLoop = 0;
 
     void Start() {
         gameObjectImage = gameObject.GetComponent<Image>();   
     }
     /// <summary>
-    /// attach the prefab's sprite to current object's sprite in image component
+    /// attach the prefab's sprite to current object's sprite in image component and render it
     /// </summary>
-    /// <param name="prefab"></param>
     public static void renderItemSprite(InventoryItem prefab) {
+        // if not currently in a dialogue: !isDialogueAction
         Image prefabImage = prefab.GetComponent<Image>();
         gameObjectImage.sprite = prefabImage.sprite;
+        itemRendered = true;
+    
     }
-
-
+   
+    private void Update() {
+        if (itemRendered) {
+           if (counter >= 0 || counter < 60) {
+                gameObjectImage.transform.Translate(0.0f, 10.0f * Time.deltaTime, 0.0f);
+                Debug.Log("if " + counter);
+                counter++;
+            }
+           if (counter >= 60) {
+                gameObjectImage.transform.Translate(0.0f, -20.0f* Time.deltaTime  , 0.0f); 
+                Debug.Log("else if " + counter);
+                counter++;
+            }
+            if (counter > 200 && animationLoop != 2) {
+                counter = 0;
+                animationLoop++;
+            }
+            else if (counter > 200 && animationLoop == 2) {
+                gameObjectImage.sprite = null;
+                itemRendered = false;
+            }
+        }
+        
+    }//end of Update
 }
