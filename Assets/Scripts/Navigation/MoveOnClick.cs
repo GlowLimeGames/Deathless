06c5ea1c930e33b33dbf637e9596d7a5bebfadc3
Sienna <animalsienna@gmail.com>
2017-11-19 21:54:44 -0500

    Reworked world obstacles for better player movement/positioning.

---
+++ Assets/Scripts/Navigation/MoveOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveOnClick : MonoBehaviour {
+    /// <summary>
+    /// When the player clicks this object, their character
+    /// should move toward the point they clicked.
+    /// </summary>
+    void OnMouseUpAsButton() {
+        if (UIManager.WorldInputEnabled) {
+            MovePlayerTowardPointer();
+        }
+    }
+
+    private void MovePlayerTowardPointer() {
+        GameItem.CancelInteraction();
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GameManager.Player.MoveToPoint(pos);
+    }
+}
