68b83b8325c38eee20fdf6db23de0b6055007b61
faithkim97 <Faith@Faiths-MacBook-Air.local>
2017-12-09 18:35:37 -0500

    currently destroyfog working--need improvements

--- Assets/Scripts/Animation/DestroyFog.cs
+++ Assets/Scripts/Animation/DestroyFog.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 /// <summary>
 /// Script used to fade away fog once door has opened
 /// </summary>
-public class DestroyFog : MonoBehaviour {
+public class DestroyFog : Fadable {
+	[SerializeField]
+	public float fadeRate;
 
 	void Update() {
 		GameObject door_open = GameObject.Find ("Door_open");
 		if (door_open != null) {
-			gameObject.SetActive (false);
+			StartCoroutine (FadeFog ());
 		}
+	}//end of Update
 
-	}
+	private IEnumerator FadeFog(){
+		yield return StartFadeOut(fadeRate, null, false);
+	}//end of FadeFog
 }
